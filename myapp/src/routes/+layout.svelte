91ada3f540ec9dabe3fe5300f98060ac8ab21905
Karen <kelenjickal@gmail.com>
2026-05-01 11:08:06 -0400

    Update +layout.svelte

--- myapp/src/routes/+layout.svelte
+++ myapp/src/routes/+layout.svelte
@@ -1,11 +1,9 @@
 <script>
 	import favicon from '$lib/assets/favicon.svg';
-
+	import '../../public/styles.css';
 	let { children } = $props();
 </script>
-
 <svelte:head>
 	<link rel="icon" href={favicon} />
 </svelte:head>
-
 {@render children()}
